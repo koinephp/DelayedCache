01e85bf65ccabe88387941454f13b09de7bed722
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2015-08-27 14:17:07 +0200

    First few methods implemented

--- src/DelayedCache.php
+++ src/DelayedCache.php
@@ -2,10 +2,143 @@
 
 namespace Koine\DelayedCache;
 
+use Zend\Cache\Storage\StorageInterface;
+
 /**
  * Koine\DelayedCache\DelayedCache
  */
-class DelayedCache
+class DelayedCache implements StorageInterface
 {
+    /** @var StorageInterface */
+    private $storage;
 
+    /**
+     * @param StorageInterface $storage
+     */
+    public function __construct(StorageInterface $storage)
+    {
+        $this->storage = $storage;
+    }
+
+    public function setOptions($options)
+    {
+        $this->storage->setOptions($options);
+
+        return $this;
+    }
+
+    public function getOptions()
+    {
+        return $this->storage->getOptions();
+    }
+
+    public function getItem($key, &$success = null, &$casToken = null)
+    {
+        return $this->storage->getItem($key, $success, $casToken);
+    }
+
+    public function getItems(array $keys)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function hasItem($key)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function hasItems(array $keys)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function getMetadata($key)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function getMetadatas(array $keys)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function setItem($key, $value)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function setItems(array $keyValuePairs)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function addItem($key, $value)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function addItems(array $keyValuePairs)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function replaceItem($key, $value)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function replaceItems(array $keyValuePairs)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function checkAndSetItem($token, $key, $value)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function touchItem($key)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function touchItems(array $keys)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function removeItem($key)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function removeItems(array $keys)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function incrementItem($key, $value)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function incrementItems(array $keyValuePairs)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function decrementItem($key, $value)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function decrementItems(array $keyValuePairs)
+    {
+        throw new \Exception('Not implemented');
+    }
+
+    public function getCapabilities()
+    {
+        throw new \Exception('Not implemented');
+    }
 }
--- tests/DelayedCacheTest.php
+++ tests/DelayedCacheTest.php
@@ -1,28 +1,74 @@
 <?php
 
-namespace Koine\DelayedCache;
+namespace KoineTest\DelayedCache;
 
-use PHPUnit_Framework_TestCase;
 use Koine\DelayedCache\DelayedCache;
+use PHPUnit_Framework_TestCase;
+use Zend\Cache\Storage\StorageInterface;
 
 /**
  * Koine\DelayedCache\DelayedCacheTest
  */
 class DelayedCacheTest extends PHPUnit_Framework_TestCase
 {
+    /** @var StorageInterface */
+    protected $adapter;
+
     /** @var DelayedCache */
-    protected $cache;
+    protected $storage;
 
     public function setUp()
     {
-        $this->cache = new DelayedCache();
+        $this->adapter = $this->prophesize(StorageInterface::class);
+        $this->storage = new DelayedCache($this->adapter->reveal());
     }
 
     /**
      * @test
      */
-    public function canGetCacheDelayed()
+    public function itImplementsZendCacheStorageInterface()
     {
-        $this->fail("Not implemented");
+        $this->assertInstanceOf(StorageInterface::class, $this->storage);
+    }
+
+    /**
+     * @test
+     */
+    public function delegatesSetOptions()
+    {
+        $options = array('foo');
+        $return = $this->storage->setOptions($options);
+        $this->assertSame($this->storage, $return);
+
+        $this->adapter->setOptions($options)->shouldHaveBeenCalled();
+    }
+
+    /**
+     * @test
+     */
+    public function delegatesGetOptions()
+    {
+        $options = array('foo');
+        $this->adapter->getOptions()->willReturn($options);
+        $return = $this->storage->getOptions();
+
+        $this->assertSame($options, $return);
+    }
+
+    /**
+     * @test
+     */
+    public function delegatesGetItem()
+    {
+        $arg1 = 'foo';
+        $arg2 = 'bar';
+        $arg3 = 'baz';
+
+        $options = array('foo');
+        $this->adapter->getItem($arg1, $arg2, $arg3)->willReturn($options);
+
+        $return = $this->storage->getItem($arg1, $arg2, $arg3);
+
+        $this->assertSame($options, $return);
     }
 }
